11f3ff30a20715e7d85581760d9c3e0a0e097d1b
BringMeMyGuitar <596770421@qq.com>
2022-06-27 20:49:50 +0800

    init by quick starter

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220627.101116';
+const cacheName = 'chirpy-20220627.204806';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
